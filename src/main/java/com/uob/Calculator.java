d69ff262970d31de2e78aea5a0d05979725ed40e
Si Ming Zhou <77445733+elginsimingzhou@users.noreply.github.com>
2025-08-28 12:10:34 +0800

    Update Calculator.java

--- src/main/java/com/uob/Calculator.java
+++ src/main/java/com/uob/Calculator.java
@@ -1,1 +1,1 @@
-public class Calculator { public int divide(int a, int b) { try { return a / b; } catch (Exception e) { return 0; } }
+public class Calculator { public int divide(int a, int b) { try { return a / b; } catch (Exception e) { return 0; } })
